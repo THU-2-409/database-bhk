236c814531dd3a6028a8bb68427927927b126869
潘立航 <plh14@mails.tsinghua.edu.cn>
2016-10-15 17:02:27 +0800

    record.h added

---
+++ record/Record.h
@@ -0,0 +1,31 @@
+//
+// Created by kinnplh on 2016/10/15.
+//
+
+#ifndef RECORD_H
+#define RECORD_H
+
+#include <string>
+#include "utils/pagedef.h"
+
+using namespace std;
+class Record {
+    int page;
+    union {
+        int offset;
+        BufType* address;
+    };
+    Table* table;
+    friend class Table;
+public:
+    int getInt(string);
+    string getString(string);
+    void setInt(string);
+    void setString(string);
+
+    Record(int _page = -1, int _offset = -1);
+
+};
+
+
+#endif //DATABASE_BHK_RECORD_H
