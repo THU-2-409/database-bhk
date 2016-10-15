54560e1b2194a6087e607baee69936c90ce3a437
lifeich0 <lifeich0@gmail.com>
2016-10-15 16:33:53 +0800

    Table类定义接口

--- table/Table.h
+++ table/Table.h
@@ -1,11 +1,24 @@
 #ifndef _H_DB_TABLE_
 #define _H_DB_TABLE_
 
-#include "Header.h"
+#include "TableHeader.h"
+#include "Record.h"
 #include <string>
+#include <list>
+using namespace std;
 
 class Table {
-    static Table create()
+public:
+    friend class Record;
+    static Table createFile(TableHeader header, string path);
+    static bool deleteFile(string path);
+    Record insertRecord(Record record);
+    bool deleteRecord(Record record);
+    bool updateRecord(Record real, Record dummy);
+    list<Record> selectRecord(Record cond);
+private:
+    BufType getBuf(int page, int offset, int size);
+    bool setBuf(int page, int offset, BufType buf, int size);
 };
 
 #endif
