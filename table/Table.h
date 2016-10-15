e57b70903475e8a3333f64a741235397d5cf41a4
lifeich0 <lifeich0@gmail.com>
2016-10-15 19:50:29 +0800

    写了好多，get/set Chars等

--- table/Table.h
+++ table/Table.h
@@ -11,13 +11,12 @@
 #include <cstring>
 #include <list>
 #include <stdio.h>
+#define PAGE_SIZE 8192
 using namespace std;
 
 class Table {
-	TableHeader th;
-	int FileID;
 public:
-    Table():memPageTop(0) {}
+    Table():memPageTop(0),bufPageManager(&fileManager) {}
     friend class Record;
     static pair<bool, Table> createFile(TableHeader header, string path);
     static int deleteFile(string path);
@@ -27,6 +26,8 @@ public:
     bool deleteRecord(Record record);
     bool updateRecord(Record real, Record dummy);
     list<Record> selectRecord(Record cond);
+
+	TableHeader th;
 private:
     char* getChars(int page, int offset, int size);
     bool setChars(int page, int offset, char* buf, int size);
@@ -35,33 +36,22 @@ private:
 
     // 非接口
     bool _preAccess(int page); // true表示正常
+    void _writeInfo2Hard(); // 将第一页信息写入文件系统
 
     FileManager fileManager;
+    BufPageManager bufPageManager;
     map<int,char*> memPages;
+    vector<char*> hardPages;
     int memPageTop;
+	int FileID;
     // 第一页,表信息，内存备份
     int recordLength, numOfColumns, recordNumber, pageNumber;
     vector<int> recordNumOfPage, availOfPage;
 };
 
-pair<bool, Table> Table::createFile(TableHeader header, string path) 
+void Table::_writeInfo2Hard() // 将第一页信息写入文件系统
 {
-    Table table;
-    if (!fileManager.createFile(path.c_str())) return make_pair(false, table);
-    table.th = header;
-    if (!table.open(path)) return make_pair(false, table);
-    int offset = 0;
-    int tmp = 0;
-    char* buf = new char[8 * 1024];
-    // 计算信息
-    table.recordLength = 0;
-    table.numOfColumns = header.getSize();
-    table.recordNumber = 0;
-    table.pageNumber = 1;
-    table.recordNumOfPage = vector<int>();
-    table.availOfPage = vector<int>();
-    for (int i = 0; i < header.getSize(); ++i)
-        table.recordLength += header[i].second;
+    char* buf = new char[PAGE_SIZE];
     // 写入缓存
     memcpy(buf, &table.recordLength, sizeof(int)); // 记录长度
     offset += sizeof(int);
@@ -90,8 +80,75 @@ pair<bool, Table> Table::createFile(TableHeader header, string path)
     table.setChars(0, 0, buf, offset);
 }
 
+pair<bool, Table> Table::createFile(TableHeader header, string path) 
+{
+    Table table;
+    if (!fileManager.createFile(path.c_str())) return make_pair(false, table);
+    table.th = header;
+    if (!table.open(path)) return make_pair(false, table);
+    int offset = 0;
+    int tmp = 0;
+    // 计算信息
+    table.recordLength = 0;
+    table.numOfColumns = header.getSize();
+    table.recordNumber = 0;
+    table.pageNumber = 1;
+    table.recordNumOfPage = vector<int>();
+    table.availOfPage = vector<int>();
+    for (int i = 0; i < header.getSize(); ++i)
+        table.recordLength += header[i].second;
+    // 写入
+    _writeInfo2Hard();
+}
+
+bool Table::_preAccess(int page)
+{
+    if (page >= 0) 
+    {
+        if (hardPages.size() <= page)
+        {
+            hardPages.insert(hardPages.end(), page - hardPages.size() + 1, NULL);
+        }
+        if (hardPages[page] == NULL)
+        {
+            int tmp;
+            hardPages[page] = (char*) bufPageManager.getPage(FileID, page, tmp);
+        }
+    } else
+        return memPages.find(page) != memPages.end();
+    return true;
+}
+
 char* Table::getChars(int page, int offset, int size)
 {
+    if (offset + size > PAGE_SIZE) return NULL;
+    if (!_preAccess(page)) return NULL;
+    char *buf;
+    if (page >= 0) buf = hardPages[page];
+    else buf = memPages[page];
+    return buf + offset;
+}
+
+bool setChars(int page, int offset, char *buf, int size)
+{
+    char *ref;
+    if (page >= 0) 
+    {
+        if (offset + size > PAGE_SIZE) return false;
+        _preAccess(page);
+        ref = hardPages[page];
+    }
+    else 
+    {
+        if (offset + size > recordLength) return false;
+        if (memPages.find(page) == memPages.end())
+        {
+            ref = new char[recordLength];
+            memPages[page] = ref;
+        } else
+            ref = memPages[page];
+    }
+    memcpy(ref + offset, buf, size);
 }
 
 int Table::deleteFile(string path){
