de03f756f093724abeedba5db3f70ac98506fb40
lifeich0 <lifeich0@gmail.com>
2016-12-24 23:30:07 +0800

    BA的比较小工具

--- utils/ByteArray.h
+++ utils/ByteArray.h
@@ -3,6 +3,7 @@
 
 #include <assert.h>
 #include <cstring>
+#include <errno>
 
 /* ByteArray
  * 字节数组封装
@@ -52,6 +53,37 @@ public:
     }
 
     int getSize() const { return this->size; }
+
+    int intCmp(ByteArray &other);
+    int strCmp(ByteArray &other);
 };
+
+int ByteArray:: intCmp(ByteArray &other)
+{
+    if (this->getSize() != 4 && other.getSize() != 4) 
+    {
+        errno = 22;
+        return 100;
+    }
+    int a = *(int*)this->buf;
+    int b = *(int*)other.buf;
+    if (a == b) return 0;
+    return a < b ? -1 : 1;
+}
+
+int ByteArray:: strCmp(ByteArray &other)
+{
+    int len = this->getSize();
+    int len1 = other.getSize();
+    if (len > len1) len = len1;
+    for (int i = 0; i < len; ++i)
+    {
+        if (buf[i] != other[i])
+            return buf[i] < other[i] ? -1 : 1;
+        if (0 == buf[i]) return 0;
+    }
+    errno = 42;
+    return 100;
+}
 
 #endif
